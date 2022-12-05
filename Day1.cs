3bf4dd92a79c5a194e01b71c53d50a15bb439014
Ethan Hutchins <ethanhutchins33@gmail.com>
2022-12-05 23:18:47 +0000

    Day1 done & refactored a bit

---
+++ Day1.cs
@@ -0,0 +1,51 @@
+namespace AOC;
+
+public class Day1
+{
+  public static List<int> GetLinesInTxtFile(string filepath)
+  {
+    List<int> intArray = new List<int>();
+
+    foreach (var line in System.IO.File.ReadAllLines(filepath))
+    {
+      intArray.Add(string.IsNullOrEmpty(line) ? 0 : int.Parse(line));
+    }
+
+    intArray.ForEach(x => Console.WriteLine(x));
+
+    return intArray;
+  }
+
+  public static void GetHighestCalories()
+  {
+    List<int> calories = GetLinesInTxtFile("day1data.txt");
+
+    int count = 0;
+    List<int> sums = new List<int>();
+
+    for (int i = 0; i < calories.Count; i++)
+    {
+      if (calories[i] != 0)
+      {
+        count += calories[i];
+      }
+      else
+      {
+        sums.Add(count);
+        //Console.WriteLine(count);
+        count = 0;
+        continue;
+      }
+    }
+
+    int largest1 = sums.Max();
+    sums.Remove(largest1);
+    int largest2 = sums.Max();
+    sums.Remove(largest2);
+    int largest3 = sums.Max();
+    sums.Remove(largest3);
+
+    Console.WriteLine("Top 3 Largest: " + (largest1 + largest2 + largest3).ToString());
+  }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace AOC;
+
+public class Program
+{
+  static void Main(string[] args)
+  {
+    Day1.GetHighestCalories();
+  }
+}
+
+
