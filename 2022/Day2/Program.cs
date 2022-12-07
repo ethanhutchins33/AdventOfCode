fb1d83b67b512dc3293e27c947ee7b9e97c89f1c
Ethan Hutchins <ethanhutchins33@gmail.com>
2022-12-07 12:39:56 +0000

    Part 2 Complete

--- 2022/Day2/Program.cs
+++ 2022/Day2/Program.cs
@@ -4,8 +4,8 @@ List<int> MyMoves = new List<int>();
 
 foreach (var line in File.ReadAllLines("./input.txt"))
 {
-  OpponentMoves.Add(GetValueOfHand(line.Substring(0, 1)));
-  MyMoves.Add(GetValueOfHand(line.Substring(2, 1)));
+  OpponentMoves.Add(GetValueOfShape(line.Substring(0, 1)));
+  MyMoves.Add(GetValueOfShape(line.Substring(2, 1)));
 }
 
 int score = 0;
@@ -32,9 +32,9 @@ int WinDrawLoseValue(int o, int m)
   }
 }
 
-int GetValueOfHand(string hand)
+int GetValueOfShape(string shape)
 {
-  return hand switch
+  return shape switch
   {
     "A" or "X" => 1,
     "B" or "Y" => 2,
@@ -43,34 +43,55 @@ int GetValueOfHand(string hand)
   };
 }
 
-//Part 2 Code
+//Part 2 Code -------------------------------------------------
 //1 = Rock
 //2 = Paper
 //3 = Scissors
 
-int WhatIShouldThrow(int ExpectedOutcome, int o)
+//X: lose, 
+//Y: draw,
+//Z: win.
+
+List<char> ExpectedOutcomes = new List<char>();
+
+foreach (var line in File.ReadAllLines("./input.txt"))
 {
-  if (ExpectedOutcome == 3) //draw
+  ExpectedOutcomes.Add(Char.Parse(line.Substring(2, 1)));
+}
+
+int WhatIShouldThrow(char ExpectedOutcome, int o)
+{
+  if (ExpectedOutcome == 'Z') //Win
+  {
+    return o switch
+    {
+      1 => 2,
+      2 => 3,
+      3 => 1,
+      _ => 0,
+    };
+  }
+  else if (ExpectedOutcome == 'X') //Lose
+  {
+    return o switch
+    {
+      1 => 3,
+      2 => 1,
+      3 => 2,
+      _ => 0,
+    };
+  }
+  else //Draw
   {
     return o;
   }
-  else if (ExpectedOutcome == 0) //lose
-  {
-    return (o + 2) % 3; //(o - ((m + 2) % 3)) == 2 //// (1 - ((3 + 2) % 3))
-  }
-  else
-  {
-    return (o + 1) % 3; //win
-  }
 }
-
-//(o - ((m + 2) % 3)) == 2 
-//(m + 2)
-
-
 
 score = 0;
 for (int i = 0; i < OpponentMoves.Count; i++)
 {
-  score += WinDrawLoseValue(OpponentMoves[i], MyMoves[i]) + MyMoves[i];
+  var myMove = WhatIShouldThrow((ExpectedOutcomes[i]), OpponentMoves[i]);
+  score += WinDrawLoseValue(OpponentMoves[i], myMove) + myMove;
 }
+
+System.Console.WriteLine($"Final Score Part 2: {score}");
