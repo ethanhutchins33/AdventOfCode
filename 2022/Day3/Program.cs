34903495a315c477cbd4efff782f29a55e5e37eb
Ethan Hutchins <ethanhutchins33@gmail.com>
2022-12-10 15:02:45 +0000

    Part 2 Complete

--- 2022/Day3/Program.cs
+++ 2022/Day3/Program.cs
@@ -54,3 +54,51 @@ int result = 0;
 repeatingChars.ForEach(x => result += ReturnValueOfChar(x));
 
 System.Console.WriteLine($"Part 1: {result}");
+
+
+// Part 2 ---------------------------------------------------------------
+
+
+List<string> ElfBagContents = new List<string>();
+
+foreach (var line in File.ReadAllLines("./input.txt"))
+{
+  ElfBagContents.Add(line);
+}
+
+List<char> Badges = new List<char>();
+bool BadgeFound = false;
+
+for (int i = 0; i < ElfBagContents.Count(); i += 3)
+{
+  BadgeFound = false;
+  foreach (var Elf1Item in ElfBagContents[i])
+  {
+    foreach (var Elf2Item in ElfBagContents[i + 1])
+    {
+      if (Elf1Item == Elf2Item)
+      {
+        foreach (var Elf3Item in ElfBagContents[i + 2])
+        {
+          if (Elf1Item == Elf2Item && Elf2Item == Elf3Item)
+          {
+            Badges.Add(Elf1Item);
+            BadgeFound = true;
+          }
+          if (BadgeFound) { break; }
+        }
+      }
+      if (BadgeFound) { break; }
+    }
+    if (BadgeFound) { break; }
+  }
+}
+
+int result2 = 0;
+
+Badges.ForEach(x =>
+{
+  result2 += ReturnValueOfChar(x);
+});
+
+System.Console.WriteLine($"Part 2: {result2}");
