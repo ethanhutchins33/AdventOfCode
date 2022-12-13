824e88ee5c992e9b9fc5c610e37ac1f26f7f126a
Ethan Hutchins <ethanhutchins33@gmail.com>
2022-12-13 01:31:11 +0000

    WIP day5, implemented stacks and pushed characters to their respective stacks. Able to pop.

--- 2022/Day5/Program.cs
+++ 2022/Day5/Program.cs
@@ -1,1 +1,37 @@
-﻿
+﻿List<string> lines = new List<string>();
+List<Stack<char>> stacks = new List<Stack<char>>();
+
+// Initialise Stacks
+for (int i = 0; i < 9; i++)
+{
+  stacks.Add(new Stack<char>());
+}
+
+// Get all lines from the input.txt file
+foreach (var line in System.IO.File.ReadAllLines("./input.txt"))
+{
+  lines.Add(line);
+}
+
+// Get characters from each line and put them into their respective stack
+for (int i = 7; i >= 0; i--)
+{
+  for (int j = 0; j < 9; j++)
+  {
+    if (char.Parse(lines[i].Substring((4 * j) + 1, 1)) != ' ')
+    {
+      stacks[j].Push(char.Parse(lines[i].Substring((4 * j) + 1, 1)));
+    }
+  }
+}
+
+for (int i = 0; i < stacks.Count; i++)
+{
+  while (stacks[i].Count != 0)
+  {
+    System.Console.WriteLine(stacks[i].Pop().ToString());
+  }
+  System.Console.WriteLine("------------------");
+}
+
+
